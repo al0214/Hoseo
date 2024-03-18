818a37d6ad220fec3fc47ffd79d8ebd7ad783e52
al0214 <sisang951@hansei.sen.hs.kr>
2024-03-18 17:27:07 +0900

    Feat : Add 0318

---
+++ C_Programming/C_Programming/ASKII_Practice.c
@@ -0,0 +1,16 @@
+//
+//  ASKII_Practice.c
+//  C_Programming
+//
+//  Created by SamoYed on 3/18/24.
+//
+
+#include <stdio.h>
+
+int main(void){
+//    printf("Hello World\n my name is \"sun\" \n address = ddd\n");
+    printf("Hello World\n");
+    printf("my name is \"sun\"\n");
+    printf("address = ddd\n");
+    return 0;
+}
