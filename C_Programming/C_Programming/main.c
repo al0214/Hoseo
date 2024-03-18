c90c75547b63352765fd1bb609952f298c0261bc
al0214 <sisang951@hansei.sen.hs.kr>
2024-03-18 17:40:34 +0900

    Feat : Xcode File

---
+++ C_Programming/C_Programming/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  C_Programming
+//
+//  Created by SamoYed on 3/18/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
