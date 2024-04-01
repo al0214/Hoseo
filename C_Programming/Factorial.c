9590b26562dc0141084a525f9e3ef1faa63899d0
지후 <165122252+imjeehoo@users.noreply.github.com>
2024-04-01 11:43:12 +0900

    Reformat Factorial.c

--- C_Programming/Factorial.c
+++ C_Programming/Factorial.c
@@ -7,23 +7,20 @@
 
 #include <stdio.h>
 
-int Fac(int a){
+int Fac(int a) {
     int b = 0;
     for (int i = 0; i <= a; ++i) {
         b = b + i;
     }
-    
+
     return b;
 }
 
-int main(void){
+int main(void) {
     int a;
-    
+
     printf("팩토리얼을 구하고 싶은 수를 입력해 주세요 : ");
     scanf("%d", &a);
-    
-    printf("구해진 팩토리얼은 %d 입니다.\n",  Fac(a));
+
+    printf("구해진 팩토리얼은 %d 입니다.\n", Fac(a));
 }
-
-
-
