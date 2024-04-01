2492a2274e6955a45130d0c7d6c98a284277f2f0
지후 <165122252+imjeehoo@users.noreply.github.com>
2024-04-01 11:42:24 +0900

    Reformat Calculator.c

--- C_Programming/Calculator.c
+++ C_Programming/Calculator.c
@@ -6,66 +6,46 @@
 //
 
 #include <stdio.h>
-int sum(int a, int b){
-    
-    return a+b;
+
+int sum(int a, int b) {
+    return a + b;
 }
 
-int minus(int a, int b){
-    
-    return a-b;
+int minus(int a, int b) {
+    return a - b;
 }
 
-int duplicate(int a, int b){
-    
-    return a*b;
+int duplicate(int a, int b) {
+    return a * b;
 }
 
-int division(int a, int b){
-    
-    return a/b;
-    
+int division(int a, int b) {
+    return a / b;
 }
 
-
-int calcu(int a, char b, int c){
-    int result = 0;
+int calcu(int a, char b, int c) {
     switch (b) {
         case '+':
-            return result  = sum(a, c);
-            break;
-            
+            return sum(a, c);
         case '-':
-            return result = minus(a, c);
-            break;
+            return minus(a, c);
         case '/':
-            return result = duplicate(a, c);
-            break;
+            return duplicate(a, c);
         case '*':
-            return result = division(a, c);
-            break;
-            
+            return division(a, c);
         default:
-            break;
+            return 0;
     }
-    
-    return result;
 }
 
-
-int main (void){
-   
-   int num1, num2;
+int main(void) {
+    int num1, num2;
     char susic;
 
+    printf("계산기 프로그램이 시작됩니다. \n");
    
-   printf("계산기 프로그램이 시작됩니다. \n");
-   
-   printf("원하는 수식을 입력해 주세요 : ");
-   scanf("%d %c %d", &num1, &susic, &num2);
-   
+    printf("원하는 수식을 입력해 주세요 : ");
+    scanf("%d %c %d", &num1, &susic, &num2);
 
     printf("%d %c %d = %d \n", num1, susic, num2, calcu(num1, susic, num2));
 };
-
-
