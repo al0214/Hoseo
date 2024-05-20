ccf363df1bf954733cd014f7361553596a1ab423
KIM JAE WOO <84881595+al0214@users.noreply.github.com>
2024-05-20 13:41:39 +0900

    Feat : array.c

---
+++ C_Programming/C_Programming/array.c
@@ -0,0 +1,59 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    srand(time(NULL));
+    int max = 0, min = 0, c=0, count=0;
+    int total[20];
+    
+    for(int i=0; i<20; i++){
+        total[i] = (rand() % 100) + 1;
+        printf("%d ",total[i]);
+    }
+    
+    printf("\n");
+    count = sizeof(total) / sizeof(int);
+    
+    printf("count = %d\n", count);
+    
+    for(int i=0; i<count; i++){
+        c += total[i];
+    }
+    
+    printf("total = %d\n", c);
+    
+    float avg;
+    
+    avg = c/count;
+    printf("avg = %lf ", avg);
+    
+    max = total[0];
+    min = total[0];
+    
+    for(int i=0; i<count; i++){
+        if(max < total[i]){
+            max = total[i];
+        }
+    }
+    printf("max = %d ", max);
+    
+    for(int i=0; i<count; i++){
+        if(total[i] < min){
+            min = total[i];
+        }
+    }
+    
+    printf("min = %d", min);
+    
+    return 0;
+}
