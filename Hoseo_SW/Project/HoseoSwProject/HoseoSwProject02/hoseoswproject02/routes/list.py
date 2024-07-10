691ca74bf572ae192bb9157d212ec2df2714a7c4
al0214 <sisang951@hansei.sen.hs.kr>
2024-07-11 07:04:12 +0900

    Feat : Project

--- Hoseo_SW/Project/HoseoSwProject/HoseoSwProject02/hoseoswproject02/routes/list.py
+++ Hoseo_SW/Project/HoseoSwProject/HoseoSwProject02/hoseoswproject02/routes/list.py
@@ -12,21 +12,26 @@ list_router = APIRouter(
 templates = Jinja2Templates(directory="./templates/")
 
 
+l = []
+
+
 @list_router.get("/", response_class=HTMLResponse)
 async def retrieve_main(request: Request):
     return templates.TemplateResponse("list.html", {
-        "request": request
+        "request": request,
+        "data": l[0]['COOKRCP01'][1]
     })
 
 
 @list_router.post("/data")
 async def get_api_para(id: apiVal):
-    url = f'http://openapi.foodsafetykorea.go.kr/api/cc1b2a587a5a42d09890/COOKRCP01/json/1/5/RCP_NM=""&RCP_PARTS_DTLS="{id.apiValue}"'
+    n = id.apiValue.strip()
+    url = f'http://openapi.foodsafetykorea.go.kr/api/cc1b2a587a5a42d09890/COOKRCP01/json/1/5/RCP_NM=""&RCP_PARTS_DTLS="{n}"'
     response = requests.get(url)
     with open("./recipe.json", 'wb') as f:
         f.write(response.content)
     j = pd.read_json("./recipe.json")
-    print(j['COOKRCP01'][1])
+    l.append(j)
     if j['COOKRCP01'][1] == {"MSG": "해당하는 데이터가 없습니다.", "CODE": "INFO-200"}:
         return {"status_code": 404}
     else:
