de7de383cfe34058d099a1a8a1cf5cba6e20cda5
al0214 <sisang951@hansei.sen.hs.kr>
2024-09-05 12:05:51 +0900

    Feat : randomPrectice

--- C_Programming/C_Programming/2_grade/shoppingInventoryManagemant.c
+++ C_Programming/C_Programming/2_grade/shoppingInventoryManagemant.c
@@ -6,3 +6,47 @@
 //
 
 #include <stdio.h>
+
+int main(void){
+        
+    int inputItem[100], outputItem[100], TypeNumber, inputID;
+    
+    printf("상풍 개수(종류)를 입력하여 주세요 : ");
+    scanf("%d", &TypeNumber);
+    
+    printf("상품 별 입고 수량을 입력하여 주세요(입력 예시 5 5 5 5 5) : ");
+    
+    for(int i = 0; i<TypeNumber-1; i++){
+        scanf("%d", &inputItem[i]);
+    }
+    
+    printf("상품 별 판매 수량을 입력하여 주세요(입력 예시 5 5 5 5 5) : ");
+    
+    for(int i = 0; i<TypeNumber-1; i++){
+        scanf("%d", &outputItem[i]);
+    }
+    
+    for (int i = 0; i<5; i++){
+        printf(<#const char *restrict, ...#>)
+    }
+        
+    printf("ID를 입력하여 주세요 : ");
+    scanf("%d", &inputID);
+    
+    printf("\n입력한 ID에 해당하는 제품의 재고 수량 => %d\n", inputItem[inputID-1]);
+    
+    for(int i = 0; i<TypeNumber; i++){
+        inputItem[i] = inputItem[i] - outputItem[i];
+    }
+    
+    printf("모든 상품의 재고 수량은 : ");
+    for (int i = 0; i<TypeNumber-1; i++) {
+        printf("%d ", inputItem[i]);
+        if(i==TypeNumber-1) printf("\n");
+    }
+    
+   
+
+    
+    return 0;
+}
