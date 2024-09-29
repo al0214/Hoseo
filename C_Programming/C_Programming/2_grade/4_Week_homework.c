dbf1e36241e4cd4e157cb69afb51f8ca97f37e10
al0214 <sisang951@hansei.sen.hs.kr>
2024-09-29 16:17:11 +0900

    Feart : 4_Week_homework

---
+++ C_Programming/C_Programming/2_grade/4_Week_homework.c
@@ -0,0 +1,17 @@
+//
+//  4_Week_homework.c
+//  C_Programming
+//
+//  Created by SamoYed on 9/29/24.
+//
+
+#include <stdio.h>
+
+int main(void){
+    
+    char message[5] = {'a', 'b', 'c', '\0'};
+    char message2[5] = {'a', 'b', 'c', 'd', '\0'};
+    
+    printf("message = %s, message2 = %s \n", message, message2);
+    
+}
