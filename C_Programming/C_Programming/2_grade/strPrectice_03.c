465c43a88a3f2165362f9f4c70c6bda11f671a6e
al0214 <sisang951@hansei.sen.hs.kr>
2024-10-04 10:38:22 +0900

    Feat : strPrecticeRebuild

--- C_Programming/C_Programming/2_grade/strPrectice_03.c
+++ C_Programming/C_Programming/2_grade/strPrectice_03.c
@@ -6,3 +6,18 @@
 //
 
 #include <stdio.h>
+#include <string.h>
+
+int main(void){
+        
+    char src[100];
+    char dest[100];
+    int result;
+//    gets(src); github보안 에러로 인하여 주석처리 하였습니다.
+//    gets(dest);
+    
+    result = strcmp(src, dest);
+    printf("result = %d\n", result);
+    
+    return 0;
+}
