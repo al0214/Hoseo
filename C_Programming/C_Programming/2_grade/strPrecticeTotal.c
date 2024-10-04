60739d5dd4392c90dc4cf86bc4c18f61e3483ec8
al0214 <sisang951@hansei.sen.hs.kr>
2024-10-04 10:38:01 +0900

    Feat : strPrectice Add

---
+++ C_Programming/C_Programming/2_grade/strPrecticeTotal.c
@@ -0,0 +1,8 @@
+//
+//  strPrecticeTotal.c
+//  C_Programming
+//
+//  Created by SamoYed on 10/4/24.
+//
+
+#include <stdio.h>
---
+++ C_Programming/C_Programming/2_grade/strPrectice_03.c
@@ -0,0 +1,8 @@
+//
+//  strPrectice_03.c
+//  C_Programming
+//
+//  Created by SamoYed on 10/4/24.
+//
+
+#include <stdio.h>
